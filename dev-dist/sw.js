d35ae2eb473427899ef32e8060adcd1982d78ca1
phuocsanh <phuocsanhtps@gmail.com>
2025-12-24 08:30:00 +0700

    build: Cập nhật revision của index.html trong precache của service worker.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-52f2a342'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.fmt5viht26o"
+    "revision": "0.43qiv1e4ai"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
